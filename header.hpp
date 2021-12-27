c48a0292a4d202886f450406d8c65fd42ec20d08
ILYA655 <75269869+ILYA655@users.noreply.github.com>
2021-12-27 06:20:32 +0300

    Update and rename notmain.cpp to header.hpp

---
+++ header.hpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+
+int main();
+int mygetch();
+void prepare(int n);
+void save_data(int n);
+void save_res();
+int gamilton (int k);
+void printGam();
+void printGam_s();
+int** create(int n);
+int** m_create(int n);
+void size_A();
+void size_M();
+void pos(int a);
+int mainmenu();
